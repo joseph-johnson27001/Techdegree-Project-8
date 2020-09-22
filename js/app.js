6a56103334764b5fd45421e82ac926128b45aba6
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2020-09-22 11:56:21 +0100

    Search Functionality
    Added Javascript Searchbox functionality

--- js/app.js
+++ js/app.js
@@ -9,6 +9,8 @@ const gridContainer = document.querySelector(".grid-container");
 const overlay = document.querySelector(".overlay");
 const modalContainer = document.querySelector(".modal-content");
 const modalClose = document.querySelector(".modal-close");
+const cards = document.getElementsByClassName("name");
+const input = document.getElementById("searchbox");
 
 
 /* ------------------------------------------------------------- */
@@ -25,12 +27,15 @@ gridContainer.addEventListener('click', e => {
   }
 });
 
-// Modal Close Click EVENT
+// Modal Close Click event listener
 
 modalClose.addEventListener('click', () => {
   overlay.classList.add("hidden");
 });
 
+// Employee Search Event Listener
+
+input.addEventListener('keyup', employeeSearch);
 
 
 /* ------------------------------------------------------------- */
@@ -38,10 +43,10 @@ modalClose.addEventListener('click', () => {
 /* ------------------------------------------------------------- */
 
 fetch(urlAPI)
-.then(res => res.json())
-.then(res => res.results)
-.then(displayEmployees)
-.catch(err => console.log(err))
+  .then(res => res.json())
+  .then(res => res.results)
+  .then(displayEmployees)
+  .catch(err => console.log(err))
 
 /* ------------------------------------------------------------- */
 /* FUNCTIONS */
@@ -85,7 +90,7 @@ function displayModal(index) {
       <p class="address">${city}</p>
       <hr />
       <p>${phone}</p>
-      <p class="address">${street}, ${state} ${postcode}</p>
+      <p class="address">${street.name}, ${state} ${postcode}</p>
       <p>Birthday:
 ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
     </div>
@@ -93,3 +98,16 @@ ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
   overlay.classList.remove("hidden");
   modalContainer.innerHTML = modalHTML;
 }
+
+// Employee Search Function
+
+ function employeeSearch() {
+  let userInput = input.value.toLowerCase();
+    for (let i = 0; i < cards[i].innerText.length; i++ ) {
+  let employeeName = cards[i].innerText.toLowerCase();
+    if(employeeName.includes(userInput)) {
+      cards[i].parentNode.parentNode.style.display = "block";
+  } else {
+    cards[i].parentNode.parentNode.style.display = "none";
+  }
+}};
